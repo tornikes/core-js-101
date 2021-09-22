8ade8bf6f899c95f24941f57f82ce585ecf326f5
tornikes <t.sadghobelashvili@gmail.com>
2021-09-22 18:33:33 +0400

    feat: implement roundToPowerOfTen

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -177,8 +177,11 @@ function getParallelipidedDiagonal(a, b, c) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  const factor = 10 ** pow;
+  let attempt = num / factor;
+  attempt = Math.round(attempt);
+  return attempt * factor;
 }
 
 /**
